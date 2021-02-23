f506a693ce8da76ce556dda090cbb0eeffa34001
WookieR <matiasroldanrma@gmail.com>
2021-02-23 09:53:44 -0300

    End of Section 5

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Grid, GridColumn, List } from "semantic-ui-react";
+import { IActivity } from '../../../app/models/activity';
+import { ActivityDetails } from "../details/ActivityDetails";
+import { ActivityForm } from "../form/ActivityForm";
+import { ActivityList } from './ActivityList';
+
+interface props {
+    activities: IActivity[];
+    selectedActivity: IActivity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: IActivity) => void;
+    deleteActivity: (id: string) => void;
+}
+
+export const ActivityDashboard = ({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity} : props) => {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} 
+                      selectActivity={selectActivity}
+                      deleteActivity={deleteActivity}
+        />
+      </Grid.Column>
+      <GridColumn width='6'>
+        {selectedActivity && !editMode &&
+        <ActivityDetails activity={selectedActivity} 
+                         cancelSelectActivity={cancelSelectActivity} 
+                         openForm={openForm}
+        />}
+        {editMode && 
+        <ActivityForm closeForm={closeForm} 
+                      activity={selectedActivity}
+                      createOrEdit={createOrEdit}
+        />} 
+      </GridColumn>
+    </Grid>
+  );
+};
